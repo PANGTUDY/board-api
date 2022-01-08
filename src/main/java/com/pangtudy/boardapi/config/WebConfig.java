6fb9bb725870dd0e1daa142885234efe61fcc9a6
JinhaJjing <tjwlsgkkgslwjt@gmail.com>
2022-01-08 13:07:02 +0900

    CORS 요청 활성화

---
+++ src/main/java/com/pangtudy/boardapi/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.pangtudy.boardapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class WebConfig implements WebFluxConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("*");
+        corsRegistry.addMapping("/board/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
